e086fd482e32d7f0500672757419f203774f15cc
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-07-24 15:35:31 +0200

    python things

---
+++ portfolio/python/Firststeps2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+spam = 1
+
+2 +2 
+
+# calculate powers
+
+5 ** 2 
+
+# round(_, 2)
+
+
+print r'C:\some\name'
+
+# 3 times 'un', followed by 'ium'
+3 * 'un' + 'ium'
+
+
+s = 'supercalifragilisticexpialidocious'
+len(s)
+
+
+letters = ['a', 'b', 'c', 'd']
+
+letters[2]
+
+# Fibonacci series:
+# the sum of two elements defines the next
+a, b = 0, 1
+
