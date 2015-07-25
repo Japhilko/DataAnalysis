90998563823781cd6916aa29d6229c7afef00864
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2015-07-25 12:07:18 +0200

    calculator second version

---
+++ portfolio/python/Calculator2.py
@@ -0,0 +1,7 @@
+def add(num1,num2):
+	return num1 + num2
+	
+def main():
+	operation = input("What do you want to do (+,-,*,/)")
+	if(operation != '+' and operatopn !='-' and operatopn !='*' and operatopn !='/')
+	print("Enter valid operation")
