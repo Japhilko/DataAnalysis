13781f6d2d593b845176d2d3e56af0bb37bbe170
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2015-07-25 11:27:17 +0200

    Hello Python

---
+++ portfolio/python/HelloWorld.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 11:20:39 2015
+
+@author: Kolb
+"""
+
+# Python Beginner Tutorial
+# https://www.youtube.com/watch?v=hFhiV5X5QM4
+
+print("Hello world")
+
+myName = input("What is your name?")
+
+print(myName)
+
