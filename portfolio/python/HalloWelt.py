b33fa6187ab93fa6a57bdadcbaf86dd651ecc0ec
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-07-27 08:57:20 +0200

    databases

---
+++ portfolio/python/HalloWelt.py
@@ -0,0 +1,9 @@
+x = input()
+
+import random
+random.seed()
+a = random.randint(1,10)
+
+
+y = a + int(x)
+print(y)
---
+++ portfolio/python/Verzweigungen.py
@@ -0,0 +1,8 @@
+y = input()
+x = int(y)
+if x > 0:
+    print("Die Zahl ist positiv")
+elif x < 0:
+    print("negativ")
+else:
+    print("Null")
