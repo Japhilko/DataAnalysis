89b5d2dbcebef1ab38eed86ae50b9694a24bcf20
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2015-07-29 20:51:58 +0200

    ipython notebook figure

---
+++ portfolio/python/ListExample.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 19:59:43 2015
+
+@author: Kolb
+"""
+
+suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
+
+first  = suitcase[0:2]  # The first and second items (index zero and one)
+middle =    suitcase[2:4]            # Third and fourth items (index two and three)
+last   =    suitcase[-1:-0]            # The last two items (index four and five)
