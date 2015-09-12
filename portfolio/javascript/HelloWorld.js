6f4b281e212a567775c73463a97e7c9e61ef3983
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2015-09-12 14:02:26 +0200

    js

--- portfolio/javascript/HelloWorld.js
+++ portfolio/javascript/HelloWorld.js
@@ -1,13 +1,34 @@
-alert("Hallo Welt"); // Kommentare 
+// alert("Hallo Welt"); // Kommentare 
+
+// https://www.youtube.com/watch?v=1Ou9jFh_-eg
 
 var anton;
 anton = "2 gleiche Schuhe"
 
-alert(anton);
+// alert(anton);
 
 var x =5 , y=10;
 
 var z;
 x + y
 
-z
+console.log(x + y);
+
+
+var wurzel = Math.sqrt(25);
+
+var neu =prompt("Gib einen Wert an");
+
+if (neu > 10) {
+	alert("Die Zahl ist größer als 10");
+}
+
+// alert(neu);
+
+alert("Du interessierst Dich für" + neu) 
+
+var a=1 ;
+while (a < 5){
+	alert("Hallo")
+	a++
+} 
