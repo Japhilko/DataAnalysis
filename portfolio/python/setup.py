5293e4c4a6f79f641f0ae9e7529163ef158aa900
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2015-09-14 23:09:47 +0200

    executable

---
+++ portfolio/python/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 22:14:10 2015
+
+@author: Kolb
+"""
+
+
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['testExec.py'])
+
+
+# https://pypi.python.org/pypi/py2exe/#installation
---
+++ portfolio/python/testExec.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 22:09:54 2015
+
+@author: Kolb
+"""
+
+# http://logix4u.net/component/content/article/27-tutorials/44-how-to-create-windows-executable-exe-from-python-script
+
+print "Python script to exe test program"
+
+
+count = 0
+
+while count < 10:
+    print "count = " + str(count) +"\n"
+    count = count + 1
+
