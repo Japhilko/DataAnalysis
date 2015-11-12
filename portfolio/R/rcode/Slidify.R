c971eca987a93ca966f3dec342d7e3617f7e01f2
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-11-12 16:15:35 +0100

    fgg

---
+++ portfolio/R/rcode/Slidify.R
@@ -0,0 +1,9 @@
+require(devtools)
+
+install_github("slidify", "ramnathv")
+install_github("slidifyLibraries", "ramnathv")
+
+library(slidify)
+
+setwd("J:/Work/Statistik/Kolb/Workshops/2015/Spatial_MA/slides")
+author("mydeck")
