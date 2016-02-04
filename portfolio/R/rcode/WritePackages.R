263a7be43c2e41bbb86ff55c479bfd472ebe8a98
Jan-Philipp Kolb <Jan-Philipp.Kolb@gesis.org>
2016-02-04 14:01:36 +0100

    update

---
+++ portfolio/R/rcode/WritePackages.R
@@ -0,0 +1,3 @@
+# http://stackoverflow.com/questions/30983013/error-in-loadnamespacei-clib-loc-libpaths-versioncheck-vii-the
+
+install.packages("stringi", repos="http://cran.rstudio.com/", dependencies=TRUE)
