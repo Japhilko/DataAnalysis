b557d832a22877865f316919caddcd7150b96524
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-03-23 17:05:31 +0100

    python links

---
+++ portfolio/python/talkpython2m.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 13:45:14 2016
+
+@author: ntuser
+"""
+
+# https://talkpython.fm/episodes/all
+
+#https://talkpython.fm/episodes/show/19/automate-the-boring-stuff-with-python
+
+import PyPDF2
