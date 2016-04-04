a90502289baccd1dacc7cef75688e3d9e8bc1133
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-04-04 19:58:21 +0200

    intro python

---
+++ portfolio/R/rcode/AddInExamples.R
@@ -0,0 +1,7 @@
+if (!requireNamespace("devtools", quietly = TRUE))
+  install.packages("devtools")
+
+devtools::install_github("rstudio/htmltools")
+devtools::install_github("rstudio/shiny")
+devtools::install_github("rstudio/miniUI")
+devtools::install_github("rstudio/addinexamples")
--- portfolio/python/talkpython2m.py
+++ portfolio/python/talkpython2m.py
@@ -10,3 +10,15 @@ Created on Wed Mar 23 13:45:14 2016
 #https://talkpython.fm/episodes/show/19/automate-the-boring-stuff-with-python
 
 import PyPDF2
+
+pdfFileObj = open('useR2015-BookOfAbstracts.pdf', 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+pdfReader.numPages
+
+pageObj = pdfReader.getPage(0)
+pageObj.extractText()
+#---------------------------------------------#
+
+# http://neckbeardrepublic.com/screencasts/pandas-vincent
