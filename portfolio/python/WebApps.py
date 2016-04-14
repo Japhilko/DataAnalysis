4f5be9f5a35912b71b4f552b2b2f703fb083ef09
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-04-14 22:59:24 +0200

    webapp

---
+++ portfolio/python/WebApps.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 21:57:48 2016
+
+@author: ntuser
+"""
+
+
+# Web Apps
+# https://azure.microsoft.com/en-us/documentation/articles/web-sites-python-ptvs-django-mysql/
+
+# https://blog.michaelckennedy.net/2014/01/03/part-2-visual-studio-and-ides-in-python-for-net-developers-series/?utm_source=devfreecasts&utm_medium=medium&utm_campaign=DevFreeCasts
