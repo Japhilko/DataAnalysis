2a6105976c709c56a6e94ebffa4979e6338463a5
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-04-15 10:16:11 +0200

    update

---
+++ portfolio/python/ScrapingWikipedia.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 09:37:19 2016
+
+@author: ntuser
+"""
+
+# http://stackoverflow.com/questions/10729116/adding-a-module-specifically-pymorph-to-spyder-python-ide
+# !pip install wikipedia
+import wikipedia
+
+zik = wikipedia.search("Zika")
+
+ny = wikipedia.page("Zika")
