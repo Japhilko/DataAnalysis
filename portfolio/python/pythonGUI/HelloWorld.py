7b254130dd85394ea47b32aea531d01d798f4057
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-04-20 22:17:35 +0200

    executable

---
+++ portfolio/python/pythonGUI/HelloWorld.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 19:29:26 2016
+
+@author: Jan-Philipp Kolb
+"""
+
+# !pip install py2exe
+
+import sys
+print ("Hello World!")
+sys.exit()
---
+++ portfolio/python/pythonGUI/easyGUI.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 19:11:52 2016
+
+@author: Jan-Philipp Kolb
+"""
+
+# http://easygui.sourceforge.net/tutorial.html#diropenbox
+
+
+# !pip install easygui
+
+import easygui
+
+easygui.ynbox('Shall I continue?', 'Title', ('Yes', 'No'))
+
+
