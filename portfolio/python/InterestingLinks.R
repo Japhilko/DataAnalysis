a7dba97ff5e38bb515f9f25625170db98c9cf996
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-04-21 23:02:15 +0200

    grinberg

---
+++ portfolio/python/InterestingLinks.R
@@ -0,0 +1,4 @@
+# Matthias Bussonnier
+# https://www.youtube.com/results?search_query=pycon+Matthias+Bussonnier
+# https://www.youtube.com/watch?v=aintdHnqaio
+# https://www.youtube.com/results?search_query=pycon+steve+dower
