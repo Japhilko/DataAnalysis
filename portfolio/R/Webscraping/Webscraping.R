c5379aaed84980cd737e280375278ae5686530c6
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-04-25 11:29:54 +0200

    webscraping

--- portfolio/R/Webscraping/Webscraping.R
+++ portfolio/R/Webscraping/Webscraping.R
@@ -1,3 +1,12 @@
 # Webscraping
 
 # install.packages("pollstR")
+
+## Wikipedia
+
+library(wikipediatrend)
+library(lattice)
+
+page_views_SG <- wp_trend("Influenza-A-Virus_H1N1",
+                          lang="de",from="2008-08-10",
+                          to="2015-11-27")
