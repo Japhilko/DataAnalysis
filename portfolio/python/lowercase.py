2cfff55f32519bb7c242a91d8eed0d3957f04812
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2016-10-20 17:17:05 +0200

    linear regression

---
+++ portfolio/python/lowercase.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 10:24:05 2016
+
+@author: kolbjp
+"""
+
+s = "MODEL SELECTION WHEN THE NUMBER OF VARIABLES EXCEEDs THE NUMBER OF OBSERVATIONS"
+print(s.lower())
+
+s= "MULTIPLE REGRESSION"
+print(s.lower())
