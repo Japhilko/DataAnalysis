b90750f8bb6c019acea9f251649ed2dd6faf04c8
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2017-02-17 15:55:36 +0100

    Dashboards

--- portfolio/R/Dashboards/GettingStartedDashboards.R
+++ portfolio/R/Dashboards/GettingStartedDashboards.R
@@ -1,1 +1,6 @@
 install.packages("flexdashboard")
+
+
+# http://rmarkdown.rstudio.com/flexdashboard/
+# https://beta.rstudioconnect.com/jjallaire/htmlwidgets-highcharter/htmlwidgets-highcharter.html
+# http://rmarkdown.rstudio.com/gallery.html
