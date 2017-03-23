1a1a1fab46e1f289bf1bb92158d89cec8318f6a8
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2017-03-23 17:10:05 +0100

    text processing

---
+++ TextAnalytics/textprocessing.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 11:37:07 2017
+
+@author: kolbjp
+"""
+
+
+
+
+# http://stackoverflow.com/questions/6797984/how-to-convert-string-to-lowercase-in-python
+s = "Kilometer"
+print(s.lower())
+
+
+# http://stackoverflow.com/questions/13635215/convert-a-string-such-that-the-first-letter-is-uppercase-and-everythingelse-is-l
+
+b = "CLAUDIA J. COULTON, TSUI CHAN, AND KRISTEN MIKELBANK"
+
+b.title()
