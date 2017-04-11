6243c49a33c00f2a8f266d6d64530ea4d1946a74
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2017-04-11 17:15:42 +0200

    youtube

---
+++ portfolio/python/MinorPy/YoutubeDL.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 09:41:53 2017
+
+@author: kolbjp
+"""
+
+# http://willdrevo.com/downloading-youtube-and-soundcloud-audio-with-python-and-pandas/
+# !pip install youtube-dl
+
+import os
+
+
+import youtube_dl
+
+
+os.chdir("D:/Daten/Privat/Musik/")
+
+options = {
+    'format': 'bestaudio/best', # choice of quality
+    'extractaudio' : True,      # only keep the audio
+    'audioformat' : "mp3",      # convert to mp3 
+    'outtmpl': '%(id)s',        # name the file the ID of the video
+    'noplaylist' : True,        # only download single song, not playlist
+}
+  # save file as the YouTube ID
+with youtube_dl.YoutubeDL(options) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=SblzGUy_sHQ'])
+
+
+###################################################
+
+import youtube_dl
+import pandas as pd
+import os
+import traceback
+
+CSV = "videos.csv"
+
+# create directory
+savedir = "mixtape"
+if not os.path.exists(savedir):
+    os.makedirs(savedir)
+
+def make_savepath(title, artist, savedir=savedir):
+    return os.path.join(savedir, "%s--%s.mp3" % (title, artist))
+
+# create YouTube downloader
+options = {
+    'format': 'bestaudio/best', # choice of quality
+    'extractaudio' : True,      # only keep the audio
+    'audioformat' : "mp3",      # convert to mp3 
+    'outtmpl': '%(id)s',        # name the file the ID of the video
+    'noplaylist' : True,}       # only download single song, not playlist
+ydl = youtube_dl.YoutubeDL(options)
+
+with ydl:
+
+    # read in videos CSV with pandas
+    df = pd.read_csv(CSV, sep=";", skipinitialspace=True)
+    df.Link = df.Link.map(str.strip)  # strip space from URLs
+
+    # for each row, download
+    for _, row in df.iterrows():
+        print "Downloading: %s from %s..." % (row.Title, row.Link)
+
+        # download location, check for progress
+        savepath = make_savepath(row.Title, row.Artist)
+        try:
+            os.stat(savepath)
+            print "%s already downloaded, continuing..." % savepath
+            continue
+
+        except OSError:
+            # download video
+            try:
+                result = ydl.extract_info(row.Link, download=True)
+                os.rename(result['id'], savepath)
+                print "Downloaded and converted %s successfully!" % savepath
+
+            except Exception as e:
+                print "Can't download audio! %s\n" % traceback.format_exc()
+
+
+
+
+###############################################
+
+# http://stackoverflow.com/questions/27473526/download-only-audio-from-youtube-video-using-youtube-dl-in-python-script
+
+from __future__ import unicode_literals
+import youtube_dl
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
